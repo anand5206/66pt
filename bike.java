1a421afe4b01b3edbcf80f6e9df748fd6e8269ff
anand5206 <ananddeepakjadhav@gmail.com>
2025-04-25 15:48:03 +0530

    Create bike.java

---
+++ bike.java
@@ -0,0 +1,25 @@
+class bike
+{ 
+       { 
+         void run(running)
+       }
+}
+class bullet extends bike
+{
+       {
+        void run(running with 50km)
+       }
+}
+class splendor extends bike
+{
+       {
+        void run(running safely with 30km)
+       }
+
+public static void main(string[] args)
+{
+bike b= new splendor;
+b.run;
+bike m= new bullet;
+m.run;
+
